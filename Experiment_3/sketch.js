39c841b043b7653b03e9a8f8e9c66806dd2d929e
LJ <lj.marquez24@bathspa.ac.uk>
2025-06-01 10:09:30 +0100

    added comments

--- Experiment_3/sketch.js
+++ Experiment_3/sketch.js
@@ -10,16 +10,16 @@ function setup() {
   //to set randomColour to be a random colour
   randomColour = color(random(255), random(255), random(255));
   
-  frameRate(1);
+  frameRate(1); //this is how fast the patterns change (the higher the number, the  quicker it will be)
 }
 
 
 
 function draw() {
   background(220);
-  generatePattern();
+  generatePattern(); //calls the function which generates the random patterns
   
-  if (counter > 2) {
+  if (counter > 2) { //timer for when the colours will change
     randomColour = color(random(255), random(255), random(255));
     
     counter = 0;
@@ -28,9 +28,9 @@ function draw() {
   counter = counter + 1;
 }
 
-function generatePattern(){
-  let space = 30;
-  for (x=0; x<width+random(30); x+=space){
+function generatePattern(){//generates the random pattern
+  let space = 30; 
+  for (x=0; x<width+random(30); x+=space){ 
     for (y=0;y<height+random(40); y+=space){
       line(x,y,x+space,y);
       line(x,y,x,y+space);
